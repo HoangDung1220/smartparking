55a6091a022d0e9717215243bbd9fc3fb585100b
HoangDung1220 <nnhoangdungdh@gmail.com>
2022-03-09 00:32:25 +0700

    front-end

---
+++ src/main/java/com/parkingsmart/ParkingSmartApplication.java
@@ -0,0 +1,13 @@
+package com.parkingsmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingSmartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingSmartApplication.class, args);
+	}
+
+}
